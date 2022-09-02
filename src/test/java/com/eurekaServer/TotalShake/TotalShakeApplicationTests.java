61a7173bb11d7316a423bb338909db0dbba44fac
silvazz99 <lucasms@ciandt.com>
2022-09-02 17:04:00 -0300

    first commit

---
+++ src/test/java/com/eurekaServer/TotalShake/TotalShakeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eurekaServer.TotalShake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TotalShakeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
